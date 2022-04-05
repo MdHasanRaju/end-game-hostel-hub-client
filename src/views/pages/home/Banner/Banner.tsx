c9ff344492713237c75513fdab83bc2c98522e8c
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-04-05 11:35:42 +0600

    text changed

--- src/views/pages/home/Banner/Banner.tsx
+++ src/views/pages/home/Banner/Banner.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import b1 from './../../../../assets/maldives-slider-img-1.jpg';
-import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@mui/material'
-import BannerDisplay from '../BannerDisplay/BannerDisplay';
 
 const Banner = () => {
-
     // const items = [
     //     {
     //         name: "Random Name #1",
@@ -21,12 +17,12 @@ const Banner = () => {
         <>
             <img style={{ width: '100%' }} src={b1} alt="" />
         </>
-    //     <Carousel>
-    //     {
-    //         items.map( (item, i) => <BannerDisplay key={i} item={item} /> )
-    //     }
-    // </Carousel>
-
+        // This is carousel part
+        //     <Carousel>
+        //     {
+        //         items.map( (item, i) => <BannerDisplay key={i} item={item} /> )
+        //     }
+        // </Carousel>
     );
 };
 
